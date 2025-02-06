c58fe1631924d733bd0191516970843a2ea72f89
MudassirAli1 <2ez4spider@gmail.com>
2025-02-07 03:19:05 +0500

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./Headerfooter/header";
-import Footer from "./Headerfooter/footer";
+
 
 
 const geistSans = Geist({
